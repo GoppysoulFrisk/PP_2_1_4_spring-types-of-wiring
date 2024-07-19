d836ac868469085ab44cf91674c0c257d7d0d437
CoreFrisk <martakova_aa@list.ru>
2024-07-19 15:10:07 +0300

    complete maybe

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,15 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    private final Deth8 deth8;
+
+    public Needle7(Deth8 deth) {
+        this.deth8 = deth;
+    }
 
     @Override
     public String toString() {
